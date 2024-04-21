485682a2ca4e72f228cb12810f2f5d2f0f955adf
Elcho911 <boss.armsport@gmail.com>
2024-04-21 23:43:59 +0600

    refactoring code

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,8 @@
-export { default as IconGadgetarium } from './icon-gadgetarium.tsx';
-export { default as IconCertificate } from './icon-сertificate.tsx';
-export { default as IconTools } from './icon-tools.tsx';
-export { default as IconCreditCard } from './icon-credit-card.tsx';
-export { default as IconHands } from './icon-hands.tsx';
-export { default as IconBigCar } from './icon-big-car.tsx';
-export { default as IconWallet } from './icon-wallet.tsx';
-export { default as IconKamazCar } from './icon-kamaz-car.tsx';
+export { default as IconGadgetarium } from './icon-gadgetarium';
+export { default as IconCertificate } from './icon-сertificate';
+export { default as IconTools } from './icon-tools';
+export { default as IconCreditCard } from './icon-credit-card';
+export { default as IconHands } from './icon-hands';
+export { default as IconBigCar } from './icon-big-car';
+export { default as IconWallet } from './icon-wallet';
+export { default as IconKamazCar } from './icon-kamaz-car';
